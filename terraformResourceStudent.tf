ecb0a39383af44ff254b9a1465a9efeb4bd84386
James Byars <jamesbyars@g.harvard.edu>
2018-01-22 15:47:25 -0500

    Remove Provisioning

--- terraformResourceStudent.tf
+++ terraformResourceStudent.tf
@@ -1,25 +1,10 @@
 resource "aws_instance" "ec2_aec_student" {
   count = 1
-  ami = "ami-80861296"
+  ami = "ami-f38abe89"
   instance_type = "m4.large"
   key_name = "${aws_key_pair.aec_key_pair.key_name}"
-  security_groups = ["${aws_security_group.aec_sg_student.name}"]
-  provisioner "remote-exec" {
-    connection {
-      type = "ssh",
-      user = "ubuntu",
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    script = "provisionStudent.sh"
-  }
-  provisioner "remote-exec" {
-    connection {
-      type = "ssh",
-      user = "ubuntu",
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    inline = ["sudo reboot now"]
-  }
+  security_groups = [
+    "${aws_security_group.aec_sg_student.name}"]
   tags {
     Name = "Agile Engineering Class Student ${format("%03d", count.index)}"
   }
